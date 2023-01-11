404dd296a203c9b2c28dacb4edd47a9a9e351a2f
Seung-Ki Kim <62886544+wlvlrtm@users.noreply.github.com>
2023-01-11 14:21:29 +0900

    무기 제작 4

--- Assets/Scripts/WeaponAR.cs
+++ Assets/Scripts/WeaponAR.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class WeaponAR : MonoBehaviour {
     [SerializeField] private WeaponSetting weaponSetting;
+    [SerializeField] private ParticleSystem muzzleFlash;
     private float lastAttackTime;
     private bool isReload;
     private Animator animator;
@@ -28,51 +29,77 @@ public class WeaponAR : MonoBehaviour {
 
     private void UpdateFire() {
         if (this.weaponSetting.isAuto) {
-            if (this.isReload) {    // 재장전 중에는 사격 불가
-                return;
-            }
-            
             if (Input.GetMouseButton(0)) {    // 조정간 자동
-                OnFire();
+                if (this.isReload) {    // 재장전 중에는 사격 불가
+                    return;
+                }
+
+                if (PlayerController.instance.isRun) {  // 달리기 도중에는 사격 불가
+                    return;
+                }
+
+                if (this.weaponSetting.currentAmmo <= 0) {  // 탄약이 고갈되면 사격 불가
+                    return;
+                }
+
+                StartCoroutine("OnFire");
             }
         }
         else if (this.weaponSetting.isSemi) {
-            if (this.isReload) {    // 재장전 중에는 사격 불가
-                return;
-            }
+            if (Input.GetMouseButtonDown(0)) {    // 조정간 반자동
+                if (this.isReload) {    // 재장전 중에는 사격 불가
+                    return;
+                }
 
-            if (Input.GetMouseButton(0)) {    // 조정간 반자동
-                OnFire();
+                if (PlayerController.instance.isRun) {  // 달리기 도중에는 사격 불가
+                    return;
+                }
+
+                if (this.weaponSetting.currentAmmo <= 0) {  // 탄약이 고갈되면 사격 불가
+                    return;
+                }
+
+                StartCoroutine("OnFire");
             }
         }
     }
 
     private void UpdateReload() {
-        if (Input.GetKeyDown(KeyCode.R)) {
-            OnReload();
+        if (Input.GetKeyDown(KeyCode.R) && this.weaponSetting.currentAmmo < this.weaponSetting.maxAmmo && this.weaponSetting.currentMagazine > 0) {
+            if (PlayerController.instance.isRun) {  // 달리기 도중에는 재장전 불가
+                return;
+            }
+
+            StartCoroutine("OnReload");
+        }
+        else if (this.weaponSetting.currentAmmo <= 0 && !this.isReload && this.weaponSetting.currentMagazine > 0) {
+            StartCoroutine("OnReload");
         }
     }
 
-    private void OnFire() {
+    private IEnumerator OnFire() {
         if (Time.time - this.lastAttackTime > this.weaponSetting.attackRate) {
-            if (PlayerController.instance.isRun) {  // 달리기 도중에는 사격 불가
-                return;
-            }
-            
-            if (this.weaponSetting.currentAmmo <= 0) {  // 탄약이 고갈되면 사격 불가
-                return;
-            }
-
             this.lastAttackTime = Time.time;
             this.weaponSetting.currentAmmo -= 1;
-            // onAmmoEvent.Invoke() 
-
+            // onAmmoEvent.Invoke()
+            this.muzzleFlash.Play();
             PlayerAnimatorController.instance.animator.Play("Fire", 1, 0);  // Animation(Fire) Play
         }
+
+        yield return null; 
     }
 
-    private void OnReload() {
+    private IEnumerator OnReload() {
+        this.isReload = true;
         PlayerAnimatorController.instance.animator.SetTrigger("Reloading");   // Animation(Reload) Play
+        this.animator.SetTrigger("Reloading");
+            
+        yield return new WaitForSeconds(3.18f);
+            
+        this.isReload = false;
         this.weaponSetting.currentAmmo = this.weaponSetting.maxAmmo;
+        this.weaponSetting.currentMagazine -= 1;
+
+        yield return null;
     }
 }
